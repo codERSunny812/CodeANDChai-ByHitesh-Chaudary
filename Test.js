6b0297048dfc14a4bff0f9601c02116fc33f1f1a
sunny <sengersunny448@gmail.com>
2023-09-16 04:09:46 +0530

    Notes Upto Lecture 2

---
+++ Test.js
@@ -0,0 +1,8 @@
+console.log("hey its me sunny ");
+
+
+// Basic understanding of the js file execution 
+
+// q-1 Diffrence between the .text file and .js file format ?
+
+ans - In  year ago whenever we have to excute the js file we first need to create a html file and add the script tag in the html file add link your js file  and excute it on any browser . But now  a days we have many software which is used to excute the js file directly with making the html file and that's NODEJS and DINOJS.
