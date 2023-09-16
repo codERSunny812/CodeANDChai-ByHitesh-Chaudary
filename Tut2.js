dff0fc1bcc94899cee4ce97d2e3a2b2a2db1ef42
sunny <sengersunny448@gmail.com>
2023-09-16 22:12:33 +0530

    lecture 2

---
+++ Tut2.js
@@ -0,0 +1,34 @@
+"use strict"
+// this is used to tell the compiler or js engine to treat all the  js codes as newer version 
+
+
+// alert(3+3) this is not be used as we are using nodejs not browser
+
+
+// try to maitain the code readability
+
+
+// Data type in the javascript 
+
+/*
+
+number = 2 to power 3
+bigInt:
+String : " "
+boolean
+null:standAlone value 
+undefined
+symbol:Unique 
+
+
+
+
+*/
+
+let name1 = "sunny"
+let age = 21
+let isLoggedIn = true
+const value = null;
+
+console.log(typeof name1)
+console.log(typeof value)  //This is an  object 
