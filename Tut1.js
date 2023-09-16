882782b0d77f7dcb3a3d0c573850210a7646054d
sunny <sengersunny448@gmail.com>
2023-09-16 12:55:47 +0530

    lecture 4

---
+++ Tut1.js
@@ -0,0 +1,20 @@
+const accountId =1245
+let accountEmail = "sengersunny@gmail.com  " 
+var accountPassword = "124"
+accountCity = "Ayodhya"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.log("Now Lets changed the variables");
+accountEmail= "sushilpandey@gmail.com"
+accountCity= "faizabad"
+accountPassword="none"
+
+
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
