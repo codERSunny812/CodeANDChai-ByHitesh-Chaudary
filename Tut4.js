963f16883a69494bb286f58a9f15fdb407387969
sunny <sengersunny448@gmail.com>
2023-09-17 12:04:27 +0530

    data type summary

---
+++ Tut4.js
@@ -0,0 +1,22 @@
+// String to number confusion 
+
+
+// operation in java script 
+
+let value  = 3;
+let negValue =-value
+console.log(negValue)
+let str1 = " hello "
+let str2 = "sunnny "
+let str3 = str1 + str2
+console.log(str3)
+
+// now lets now about some complex sitution 
+
+console.log("1" + "2")
+console.log("1" + 2);
+console.log("1"+2+3);
+console.log(1+"2");
+console.log(1+2+"3");
+
+// now about prefix and postfix
