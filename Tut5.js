5813d23537aa66cee043da1fc26fdeb8d0202366
sunny <sengersunny448@gmail.com>
2023-09-17 12:05:46 +0530

    comparision data type

---
+++ Tut5.js
@@ -0,0 +1,20 @@
+// Data Type comparision
+
+console.log(2>3) //false
+console.log(3<1) //false
+
+console.log('2'>1) //true
+console.log('-2'<0)//true
+
+console.log(null > 0) //false
+console.log(null == 0) //false
+console.log(null >= 0) //false
+
+
+/*
+NOTE:
+The working of the comparision and equal operator is completly diffrent .
+comparision convert the null into a number and treated it as 0 .
+
+*/
+
