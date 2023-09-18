0ff08eac976bc6030e4a93c0da601e5f477b3d11
sunny <sengersunny448@gmail.com>
2023-09-18 11:39:53 +0530

    Date and Time

---
+++ Tut9.js
@@ -0,0 +1,26 @@
+// Number and maths in js 
+
+ const score= 400;
+ console.log(score);
+ const balance = new Number(100);
+ console.log(balance);
+
+ const balanceNew = new Number(100.9123);
+ console.log(balanceNew);
+ console.log(balanceNew.toFixed(2));
+ console.log(balanceNew.toPrecision(4));
+ console.log(balanceNew.toLocaleString());
+
+//  Maths in the javascript
+console.log("maths in js ");
+console.log(Math);
+console.log(Math.abs(-12));
+console.log(Math.round(4.6));
+console.log(Math.round(4.1));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.6));
+console.log(Math.max(1,2,3,4,7));
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) +1);
+console.log((Math.floor(Math.random()*10)));
