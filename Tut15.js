62b84be524769eae9f2761f17fe5857f5b0817f9
sunny <sengersunny448@gmail.com>
2023-09-22 01:33:20 +0530

    how js excute

---
+++ Tut15.js
@@ -0,0 +1,24 @@
+// how does javascript excute 
+//   *****important for interview ****
+/*
+
+ execution context 
+
+when javascript scan a file , it make an environment that's called execution context and that handle the entire execution of the code. 
+
+There are basically to two of execution context. 
+
+1. global execution context 
+### which is created when the javascript file first start to run. 
+2. function excution context 
+### A function execution is created whenever a function is called or invoked 
+
+
+********Phases in the  javascript execution context **********
+
+1.memory  creation phase (variable value is equal to undefined)
+2. execution  phase
+
+
+*/
+
