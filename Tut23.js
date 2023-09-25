128a390db27c7ab47ad69f80db38442b30c035be
sunny <sengersunny448@gmail.com>
2023-09-26 01:13:18 +0530

    Async in javascript

---
+++ Tut23.js
@@ -0,0 +1,4 @@
+// Asyn Code  in javascript 
+// Javascript is a syncronus and single threaded .its by default synchronus lang  but we can make it async using many other functions 
+
+// it execute one line at a  time N
